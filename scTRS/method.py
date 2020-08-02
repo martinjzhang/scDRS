3775b209d03bc48247c0619ca58698d494ebb4ed
Kangcheng Hou <kangchenghou@gmail.com>
2020-08-02 21:11:18 +0000

    add gearys_C

--- scTRS/method.py
+++ scTRS/method.py
@@ -133,6 +133,52 @@ def score_cell_kangcheng_072920(data,
     
     return adata if copy else None
 
+def gearys_c(adata, vals):
+    """Compute Geary's C statistics for an AnnData
+    Adopted from https://github.com/ivirshup/scanpy/blob/metrics/scanpy/metrics/_gearys_c.py
+    
+    C =
+        \frac{
+            (N - 1)\sum_{i,j} w_{i,j} (x_i - x_j)^2
+        }{
+            2W \sum_i (x_i - \bar{x})^2
+        }
+    
+    Args:
+        adata (AnnData): AnnData object
+            adata.obsp["Connectivities] should contain the connectivity graph, 
+            with shape `(n_obs, n_obs)`
+        vals (Array-like):
+            Values to calculate Geary's C for. If one dimensional, should have
+            shape `(n_obs,)`. 
+    """
+    graph = adata.obsp["connectivities"]
+    assert graph.shape[0] == graph.shape[1]
+    graph_data = graph.data.astype(np.float_, copy=False)
+    assert graph.shape[0] == vals.shape[0]
+    assert(np.ndim(vals) == 1)
+    
+    W = graph_data.sum()
+    N = len(graph.indptr) - 1
+    vals_bar = vals.mean()
+    vals = vals.astype(np.float_)
+    
+    # numerators
+    total = 0.0
+    for i in range(N):
+        s = slice(graph.indptr[i], graph.indptr[i + 1])
+        # indices of corresponding neighbors
+        i_indices = graph.indices[s]
+        # corresponding connecting weights
+        i_data = graph_data[s]
+        total += np.sum(i_data * ((vals[i] - vals[i_indices]) ** 2))
+
+    numer = (N - 1) * total
+    denom = 2 * W * ((vals - vals_bar) ** 2).sum()
+    C = numer / denom
+        
+    return C
+
 def get_sparse_var(sparse_X, axis=0):
     
     v_mean = sparse_X.mean(axis=axis)
