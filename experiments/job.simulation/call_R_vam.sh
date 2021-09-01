c235fcd46ad5f24acf5567a2097aa4bf5f32519a
Martin Zhang <martinjzhang@gmail.com>
2021-09-01 15:23:23 -0400

    job.case_hep

--- experiments/job.simulation/call_R_vam.sh
+++ experiments/job.simulation/call_R_vam.sh
@@ -4,15 +4,15 @@
 #SBATCH -t 0-6:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared   # Partition to submit to
 #SBATCH --mem=32000           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --array=4,8,12,16           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o /n/home11/mjzhang/gwas_informed_scRNAseq/scTRS/experiments/job_info/job_%A_%a.out # Standard output
-#SBATCH -e /n/home11/mjzhang/gwas_informed_scRNAseq/scTRS/experiments/job_info/job_%A_%a.err # Standard error
+#SBATCH --array=1-12           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o /n/home11/mjzhang/gwas_informed_scRNAseq/scDRS/experiments/job_info/job_%A_%a.out # Standard output
+#SBATCH -e /n/home11/mjzhang/gwas_informed_scRNAseq/scDRS/experiments/job_info/job_%A_%a.err # Standard error
 
 I_LINE=$SLURM_ARRAY_TASK_ID
 # I_LINE=3
 DNAME=tms_facs.ncell_10k
 
-DATA_PATH=/n/holystore01/LABS/price_lab/Users/mjzhang/scTRS_data/simulation_data
+DATA_PATH=/n/holystore01/LABS/price_lab/Users/mjzhang/scDRS_data/simulation_data
 GS_NAME=$(head -n $I_LINE ${DATA_PATH}/simu_list.txt | tail -n 1)
 GS_FILE=${DATA_PATH}/gs_file/${GS_NAME}.gs
 SC_FILE=${DATA_PATH}/single_cell_data/${DNAME}.tsv
--- experiments/job.simulation/compute_simu_score_vision.sh
+++ experiments/job.simulation/compute_simu_score_vision.sh
@@ -4,25 +4,25 @@
 #SBATCH -t 0-0:30          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared   # Partition to submit to
 #SBATCH --mem=32000           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --array=1-16           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o /n/home11/mjzhang/gwas_informed_scRNAseq/scTRS/experiments/job_info/job_%A_%a.out # Standard output
-#SBATCH -e /n/home11/mjzhang/gwas_informed_scRNAseq/scTRS/experiments/job_info/job_%A_%a.err # Standard error
+#SBATCH --array=1-12           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o /n/home11/mjzhang/gwas_informed_scRNAseq/scDRS/experiments/job_info/job_%A_%a.out # Standard output
+#SBATCH -e /n/home11/mjzhang/gwas_informed_scRNAseq/scDRS/experiments/job_info/job_%A_%a.err # Standard error
 
 I_LINE=$SLURM_ARRAY_TASK_ID
 # I_LINE=12
 DNAME=tms_facs.ncell_10k
 
-DATA_PATH=/n/holystore01/LABS/price_lab/Users/mjzhang/scTRS_data/simulation_data
+DATA_PATH=/n/holystore01/LABS/price_lab/Users/mjzhang/scDRS_data/simulation_data
 GS_NAME=$(head -n $I_LINE ${DATA_PATH}/simu_list.txt | tail -n 1)
 GS_FILE=${DATA_PATH}/gs_file/${GS_NAME}.gs
 H5AD_FILE=${DATA_PATH}/single_cell_data/${DNAME}.h5ad
-COV_FILE=/n/holystore01/LABS/price_lab/Users/mjzhang/scTRS_data/tabula_muris_senis/tms_facs.cov
+COV_FILE=/n/holystore01/LABS/price_lab/Users/mjzhang/scDRS_data/tabula_muris_senis/tms_facs.cov
 
 OUT_FOLDER=${DATA_PATH}/score_file/result_vision/${DNAME}.${GS_NAME}
 
 [ -d ${OUT_FOLDER} ] || mkdir ${OUT_FOLDER}
 
-python3 /n/home11/mjzhang/gwas_informed_scRNAseq/scTRS/compute_score_vision.py \
+python3 /n/home11/mjzhang/gwas_informed_scRNAseq/scDRS/compute_score_vision.py \
     --h5ad_file $H5AD_FILE\
     --h5ad_species mouse\
     --cov_file $COV_FILE\
