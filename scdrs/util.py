523edde9f3d5971bd01a31ede80e0802394d9718
Kangcheng Hou <kangchenghou@gmail.com>
2021-09-02 12:51:40 -0400

    clean up deprecated file gene_score.py and deprecated functions in util.py

--- scdrs/util.py
+++ scdrs/util.py
@@ -85,66 +85,6 @@ def qqplot(x, y, quantiles=None, interpolation='nearest', ax=None, **kwargs):
     ax.scatter(x_quantiles, y_quantiles, **kwargs)
     
 
-# # Can we delete this one?
-def empirical_zsc(score, null):
-    """
-        Calculate the empirical p-value for two arrays. 
-        For each element in `score`, count how many elements in the null are below the score, 
-            and derive a p-value.
-        This is for a somewhat better implementation.
-        
-        score: Array-like, e.g., the TRS score
-        null: Array-like, e.g., null distribution
-        
-        Returns empirical p-value, same-length as null
-    """
-    df = pd.DataFrame({
-        'id': np.concatenate([
-            [f'score_{i}' for i in np.arange(len(score))],
-            [f'null_{i}' for i in np.arange(len(null))]]),
-        'val': np.concatenate([score, null]),
-        'is_null': np.concatenate([np.zeros(len(score)), np.ones(len(null))])
-    })
-    df = df.sort_values('val')
-    # TODO: does these pseudo-count makes sense?
-    df['cum_prop'] = (np.cumsum(df['is_null']) + 1) / (len(null) + 2)
-    df = df[df['id'].str.startswith('score_')].sort_index()
-    return sp.stats.norm.ppf(df['cum_prop'].values)
-
-
-# Can we delete this one?
-# https://github.com/scikit-learn/scikit-learn/blob/0fb307bf3/sklearn/preprocessing/_data.py#L1092
-def sparse_robust_scale(X, quantile_range=(25.0, 75.0), copy=True):
-    """
-        sparse_robust_scale:
-        Compute the robust scale for matri X
-        
-        X: sparse matrix, rows are individuals, columns are genes,
-        
-        Return: Compute a scale for each gene
-    """
-    assert sp.sparse.issparse(X), "X must be a sparse matrix"
-    # at fit, convert sparse matrices to csc for optimized computation of the quantiles
-    X = X.tocsc(copy=copy)
-    q_min, q_max = quantile_range
-    if not 0 <= q_min <= q_max <= 100:
-        raise ValueError("Invalid quantile range: %s" %
-                         str(quantile_range))
-    
-    quantiles = []
-    for feature_idx in range(X.shape[1]):
-        column_nnz_data = X.data[X.indptr[feature_idx]:
-                                 X.indptr[feature_idx + 1]]
-        column_data = np.zeros(shape=X.shape[0], dtype=X.dtype)
-        column_data[:len(column_nnz_data)] = column_nnz_data
-
-        quantiles.append(np.nanpercentile(column_data, quantile_range))
-
-    quantiles = np.transpose(quantiles)
-
-    return quantiles[1] - quantiles[0]
-
-
 def gearys_c(adata, vals):
     """Compute Geary's C statistics for an AnnData
     Adopted from https://github.com/ivirshup/scanpy/blob/metrics/scanpy/metrics/_gearys_c.py
@@ -291,58 +231,6 @@ def compute_gearysc_significance(
         -(df_rls[trait_col].values - df_rls["ctrl_mean"]) / df_rls["ctrl_std"]
     )
     return df_rls
-
-
-# Can we delete this one?
-def calculate_trs_stats(zsc_dict, zsc_index, stats_dict, adata, stratify_by):
-    """
-    Calculate statistics of TRS stratified by covariate, e.g., celltype, 
-    this function only cope with simple statistics such as mean, sd that
-    takes an array of unordered TRS and outputs a single scalar.
-    # Arguments
-    zsc_dict: z-score dict, trait -> z-score array, sorted according to zsc_index
-    zsc_index: index for the z-score in `zsc_dict`
-    stats_dict: statistics to be computed, name -> function,
-        the function takes an array and output a scalar
-    adata: AnnData for the meta information
-    stratify_by: which column in `meta_df` to stratify
-    # TODO
-    1. Add permutation procedure
-    """
-    assert stratify_by in ["tissue_celltype", "cell_ontology_class"]
-    
-    meta_df = adata.obs.copy()
-    trait_list = list(zsc_dict.keys())
-    zsc_df = meta_df.join(pd.DataFrame(zsc_dict, index=zsc_index), how="inner")
-    
-    stratify_list = sorted(zsc_df[stratify_by].unique())
-    rls_df_dict = {stats: pd.DataFrame(index=stratify_list, columns=trait_list, data=0) for stats in stats_dict}
-    
-    for strata, strata_df in zsc_df.groupby(stratify_by):
-        strata_n_cell = strata_df.shape[0]
-        for stats_name in stats_dict:
-            stats_fun = stats_dict[stats_name]
-            if stats_fun is not None:
-                for trait in trait_list:
-                    rls_df_dict[stats_name].loc[strata, trait] = stats_fun(strata_df[trait].values)
-            else:
-                if stats_name == "gearysc":
-                    for trait in trait_list:
-                        rls_df_dict[stats_name].loc[strata, trait] = gearys_c(adata[strata_df.index], strata_df[trait].values)
-    return rls_df_dict
-
-#         gc_pval = get_p_from_empi_null(-gc, -np.array(gc_null_dist))
-#         rls_dict.setdefault('gearys_c', []).append(gc)
-#         rls_dict.setdefault('gearys_c_pval', []).append(gc_pval)
-    
-#             for perm_i in range(num_perms):
-#                 perm_cells = np.random.choice(trs_df.index, size=ct_num_cells, replace=False)
-#                 fun_null_dist.append(fun(trs_df.loc[perm_cells, 'trs_ez']))
-                
-#             fun_pval = get_p_from_empi_null(fun_rls, fun_null_dist)
-            
-#             rls_dict.setdefault(f'{fun_name}', []).append(fun_rls)
-#             rls_dict.setdefault(f'{fun_name}_pval', []).append(fun_pval)
 
 
 def zsc2pval(zsc):
