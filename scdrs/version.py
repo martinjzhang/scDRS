1ad1f26043582b81687f658dd4580ce5acd93401
Martin <martinjzhang@gmail.com>
2021-12-25 15:05:47 -0500

    Add scdrs/version.py

---
+++ scdrs/version.py
@@ -0,0 +1,6 @@
+# Store the version here so:
+# 1) we don't load dependencies by storing it in __init__.py
+# 2) we can import it in setup.py for the same reason
+# 3) we can import it into your module module
+__version__ = '1.0.0'
+__version_info__ = tuple([ int(num) for num in __version__.split('.')])
